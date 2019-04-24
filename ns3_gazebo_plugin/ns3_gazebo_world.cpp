ff90a90ba16db51e5a831dfd5040acd045c896d2
Allen, Bruce (CIV) <bdallen@nps.edu>
2019-04-24 12:29:56 -0700

    add world plugin

--- ns3_gazebo_plugin/ns3_gazebo_world.cpp
+++ ns3_gazebo_plugin/ns3_gazebo_world.cpp
@@ -8,9 +8,64 @@
 
 #include "ns3/core-module.h"
 #include "ns3/node-container.h"
-#include "ns3_setup.hpp"
+#include "ns3/network-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/tap-bridge-module.h"
 
 namespace ns3_gazebo_world {
+
+static const int COUNT=5;
+
+void ns3_setup(ns3::NodeContainer& ns3_nodes) {
+
+  // run ns3 real-time with checksums
+  ns3::GlobalValue::Bind("SimulatorImplementationType",
+                          ns3::StringValue("ns3::RealtimeSimulatorImpl"));
+  ns3::GlobalValue::Bind("ChecksumEnabled", ns3::BooleanValue(true));
+
+  // Create ns3_nodes
+  ns3_nodes.Create(COUNT);
+
+  // Wifi settings
+  ns3::WifiHelper wifi;
+  wifi.SetStandard(ns3::WIFI_PHY_STANDARD_80211a);
+  wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
+                          "DataMode", ns3::StringValue("OfdmRate54Mbps"));
+
+  // ad-hoc Wifi network
+  ns3::WifiMacHelper wifiMac;
+  wifiMac.SetType("ns3::AdhocWifiMac");
+
+  // physical layer
+  ns3::YansWifiChannelHelper wifiChannel(ns3::YansWifiChannelHelper::Default());
+  ns3::YansWifiPhyHelper wifiPhy(ns3::YansWifiPhyHelper::Default());
+  wifiPhy.SetChannel(wifiChannel.Create());
+
+  // Install the wireless devices onto our ghost ns3_nodes.
+  ns3::NetDeviceContainer devices = wifi.Install(wifiPhy, wifiMac, ns3_nodes);
+
+  // antenna locations
+  ns3::Ptr<ns3::ListPositionAllocator>positionAlloc =
+                         ns3::CreateObject<ns3::ListPositionAllocator>();
+  for (int i=0; i<COUNT; i++) {
+    positionAlloc->Add(ns3::Vector(0.0, 0.0, 0.0));
+  }
+  ns3::MobilityHelper mobility;
+  mobility.SetPositionAllocator(positionAlloc);
+  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+  mobility.Install(ns3_nodes);
+
+  // connect Wifi through TapBridge devices
+  ns3::TapBridgeHelper tapBridge;
+  tapBridge.SetAttribute("Mode", ns3::StringValue("UseLocal"));
+  char buffer[10];
+  for (int i=0; i<COUNT; i++) {
+    sprintf(buffer, "wifi_tap%d", i+1);
+    tapBridge.SetAttribute("DeviceName", ns3::StringValue(buffer));
+    tapBridge.Install(ns3_nodes.Get(i), devices.Get(i));
+  }
+}
 
 static void ns3_thread_function(void) {
   std::cout << "Starting ns-3 Wifi simulator in thread.\n";
@@ -44,7 +99,7 @@ class NS3GazeboWorld : public gazebo::WorldPlugin {
   void OnUpdate(const gazebo::common::UpdateInfo& _info) {
     ignition::math::Pose3d pose = model_ptr->WorldPose();
     float x = pose.Pos().X();
-    std::cout << "OnUpdate Point.x: " << x << "\n";
+//    std::cout << "OnUpdate Point.x: " << x << "\n";
   }
 
   void Init() {
