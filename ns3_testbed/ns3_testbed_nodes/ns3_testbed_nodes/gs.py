ad8d26de6f34678924eb8b80c2777cb90fa9b16a
Allen, Bruce (CIV) <bdallen@nps.edu>
2019-06-25 18:37:53 -0700

    add pipe logger

--- ns3_testbed/ns3_testbed_nodes/ns3_testbed_nodes/gs.py
+++ ns3_testbed/ns3_testbed_nodes/ns3_testbed_nodes/gs.py
@@ -4,11 +4,16 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from ns3_testbed_nodes.testbed_codec import testbed_decode
+from ns3_testbed_nodes.pipe_logger import PipeLogger, PIPE_NAME
 
 class GS(Node):
 
-    def __init__(self):
+    def __init__(self, use_pipe_logger):
         super().__init__('groundstation')
+        self.use_pipe_logger = use_pipe_logger
+        if use_pipe_logger:
+            self.pipe_logger = PipeLogger()
+
         self.sub = self.create_subscription(String, 'odometry',
                                             self.subscription_callback)
         self.sub = self.create_subscription(String, 'image',
@@ -17,15 +22,21 @@ class GS(Node):
     def subscription_callback(self, msg):
         source, name, number, size, dt = testbed_decode(msg.data)
         response = "%s,%s,%d,%d,%f"%(source, name, number, size, dt)
-        self.get_logger().info(response)
+        if self.use_pipe_logger:
+            self.pipe_logger.log(response)
+        else:
+            # use Node's native logger
+            self.get_logger().info(response)
 
-def main(args=None):
+def main():
     parser = ArgumentParser(description="Testbed ground station (GS).")
+    parser.add_argument("-p","--use_pipe_logger", action="store_true",
+                        help="Send output to pipe '%s'"%PIPE_NAME)
     args = parser.parse_args()
 
     rclpy.init()
 
-    node = GS()
+    node = GS(args.use_pipe_logger)
     rclpy.spin(node)
 
     node.destroy_node()
---
+++ ns3_testbed/ns3_testbed_nodes/ns3_testbed_nodes/pipe_logger.py
@@ -0,0 +1,46 @@
+# Provides PipeLogger and PipeReader
+import stat, os
+from os.path import expanduser
+from fcntl import flock, LOCK_EX, LOCK_UN
+from threading import Thread
+from queue import Queue
+
+PIPE_NAME=os.path.join(expanduser("~"), "_testbed_pipe.pipe")
+def _maybe_make_pipe():
+    if os.path.exists(PIPE_NAME) and stat.S_ISFIFO(os.stat(PIPE_NAME).st_mode):
+        # path exists and is pipe
+        return
+    else:
+        # create pipe
+        os.mkfifo(PIPE_NAME)
+
+# use PipeLogger to threadsafely write to pipe
+class PipeLogger():
+
+    def __init__(self):
+        _maybe_make_pipe()
+        self.f = open(PIPE_NAME, "w")
+
+    def log(self, text):
+        f=self.f
+        # secure exclusive lock, write, flush, unlock
+        flock(f, LOCK_EX)
+        print(text,file=f) # print appends \n
+        f.flush()
+#        print("log text: %s"%text)
+        flock(f, LOCK_UN)
+
+# use PipeReader to consume pipe to queue
+def _pipe_consumer_thread(queue):
+    with open(PIPE_NAME) as f:
+        while True:
+            line = f.readline()
+#            print("read then enqueue line: %s"%line)
+            queue.put(line)
+class PipeReader():
+    def __init__(self):
+        _maybe_make_pipe()
+        self.queue = Queue()
+        t = Thread(target=_pipe_consumer_thread, args=(self.queue,))
+        t.start()
+
